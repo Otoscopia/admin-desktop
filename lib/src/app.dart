391ec75cfc993b34e181df1f629e6c96da114a24
Usha Nicole Cobrado <ushanicole0421@gmail.com>
2025-05-02 21:29:03 +0800

    ✨ feat: Add `app.dart` for application entry-point

---
+++ lib/src/app.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/foundation.dart';
+
+import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+import 'package:system_theme/system_theme.dart';
+
+import 'package:admin/src/core/index.dart';
+import 'package:admin/src/feature/admin.dart';
+import 'package:admin/src/feature/authentication/sign_in.dart';
+
+class MyApp extends ConsumerStatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  ConsumerState<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    final theme = ref.watch(appThemeProvider);
+    final accentColor = ref.watch(currentAccentColorProvider).toAccentColor();
+    final accent =
+        theme.systemAccent
+            ? accentColor
+            : SystemTheme.fallbackColor.toAccentColor();
+
+    return FluentApp(
+      title: 'Otoscopia Admin',
+      themeMode: theme.mode,
+      debugShowCheckedModeBanner: false,
+      theme: FluentThemeData.light().copyWith(accentColor: accent),
+      darkTheme: FluentThemeData.dark().copyWith(accentColor: accent),
+      navigatorObservers: !kDebugMode ? [SentryNavigatorObserver()] : [],
+      home:
+          kIsWeb
+              ? FutureBuilder(
+                future: null,
+                builder: (context, snapshot) {
+                  final auth = ref.watch(authenticationProvider);
+
+                  return PageContainer(
+                    content: auth.user != null ? Admin() : SignIn(),
+                  );
+                },
+              )
+              : AppContainer(
+                onLoaded: (context) {
+                  final auth = ref.watch(authenticationProvider);
+
+                  return PageContainer(
+                    content: auth.user != null ? Admin() : SignIn(),
+                  );
+                },
+              ),
+    );
+  }
+}
