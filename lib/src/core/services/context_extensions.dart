9ee58b242cf3053b60eccc8ddb46be8c129e615f
Usha Nicole Cobrado <ushanicole0421@gmail.com>
2025-05-02 23:02:16 +0800

    ✨ feat: Add build context extensions

---
+++ lib/src/core/services/context_extensions.dart
@@ -0,0 +1,34 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+extension NavigationExtension on BuildContext {
+  FluentThemeData get theme => FluentTheme.of(this);
+
+  Brightness get brightness => FluentTheme.of(this).brightness;
+
+  bool get isDark => FluentTheme.of(this).brightness.isDark;
+
+  push(Widget page) {
+    return Navigator.of(this).push(FluentPageRoute(builder: (context) => page));
+  }
+
+  pushWithAnimation(Widget page) {
+    return Navigator.of(this).push(
+      PageRouteBuilder(
+        pageBuilder: (context, animation, secondaryAnimation) {
+          return page;
+        },
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          const begin = Offset(0.0, 1.0);
+          const end = Offset.zero;
+          final tween = Tween(begin: begin, end: end);
+          final offsetAnimation = animation.drive(tween);
+          return SlideTransition(position: offsetAnimation, child: child);
+        },
+      ),
+    );
+  }
+
+  pop() {
+    return Navigator.of(this).pop();
+  }
+}
