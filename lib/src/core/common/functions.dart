13c88a07c6e3f6a687eb997ed2c37f5b1785e0a8
Usha Nicole Cobrado <ushanicole0421@gmail.com>
2025-05-02 21:31:38 +0800

    ✨ feat: Create common functions for getting collections, functions, status, and events

---
+++ lib/src/core/common/functions.dart
@@ -0,0 +1,25 @@
+import 'package:admin/src/core/index.dart';
+
+String get databaseId => collectionIds['database'];
+
+String getCollectionId(String name) {
+  return List.from(
+    collectionIds['collections'],
+  ).firstWhere((e) => e['name'] == name)['id'];
+}
+
+String getFunctionId(String name) {
+  return List.from(
+    functionIds['functions'],
+  ).firstWhere((e) => e['name'] == name)['id'];
+}
+
+Status getStatus(String status) {
+  return Status.values.firstWhere((e) => e.name == status);
+}
+
+String geteventId(String activity) {
+  return List.from(
+    eventIds['events'],
+  ).firstWhere((e) => e['activity'] == activity)['id'];
+}
