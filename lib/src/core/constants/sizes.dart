f29a8b2e7a207337c35f28b158093421d3ed0568
Usha Nicole Cobrado <ushanicole0421@gmail.com>
2025-05-02 23:01:43 +0800

    ✨ feat: Add constants

---
+++ lib/src/core/constants/sizes.dart
@@ -0,0 +1,25 @@
+import 'package:gap/gap.dart';
+
+class Sizes {
+  static const p4 = 4.0;
+  static const p8 = 8.0;
+  static const p12 = 12.0;
+  static const p16 = 16.0;
+  static const p20 = 20.0;
+  static const p24 = 24.0;
+  static const p32 = 32.0;
+  static const p40 = 40.0;
+  static const p48 = 48.0;
+  static const p64 = 64.0;
+}
+
+const gap4 = Gap(Sizes.p4);
+const gap8 = Gap(Sizes.p8);
+const gap12 = Gap(Sizes.p12);
+const gap16 = Gap(Sizes.p16);
+const gap20 = Gap(Sizes.p20);
+const gap24 = Gap(Sizes.p24);
+const gap32 = Gap(Sizes.p32);
+const gap40 = Gap(Sizes.p40);
+const gap48 = Gap(Sizes.p48);
+const gap64 = Gap(Sizes.p64);
