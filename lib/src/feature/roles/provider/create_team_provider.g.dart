dbb62df4f71c81409a98d40c6a8fcac1920e2576
Usha Nicole Cobrado <ushanicole0421@gmail.com>
2025-05-03 14:50:51 +0800

    ✨ feat: Add create team provider

---
+++ lib/src/feature/roles/provider/create_team_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'create_team_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$createTeamsHash() => r'02c91e8e135fd63f1bc56f9830a70fd82da6c200';
+
+/// See also [CreateTeams].
+@ProviderFor(CreateTeams)
+final createTeamsProvider = NotifierProvider<CreateTeams, TeamData>.internal(
+  CreateTeams.new,
+  name: r'createTeamsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$createTeamsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CreateTeams = Notifier<TeamData>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
