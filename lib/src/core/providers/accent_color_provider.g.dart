b6c1ff2a836a7e975664d472f8edc64d67985314
Usha Nicole Cobrado <ushanicole0421@gmail.com>
2025-05-03 14:22:02 +0800

    🔧 chore: Upload generated files

---
+++ lib/src/core/providers/accent_color_provider.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'accent_color_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$systemAccentColorHash() => r'82396a26a87ef832a59a14b4be57a2af718f73e6';
+
+/// See also [systemAccentColor].
+@ProviderFor(systemAccentColor)
+final systemAccentColorProvider = StreamProvider<Color>.internal(
+  systemAccentColor,
+  name: r'systemAccentColorProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$systemAccentColorHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef SystemAccentColorRef = StreamProviderRef<Color>;
+String _$currentAccentColorHash() =>
+    r'1495693dceea0465b587294f8201f79e2db81f9b';
+
+/// See also [currentAccentColor].
+@ProviderFor(currentAccentColor)
+final currentAccentColorProvider = AutoDisposeProvider<AccentColor>.internal(
+  currentAccentColor,
+  name: r'currentAccentColorProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$currentAccentColorHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef CurrentAccentColorRef = AutoDisposeProviderRef<AccentColor>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
---
+++ lib/src/core/providers/authentication_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'authentication_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$authenticationHash() => r'544bb1d78365cbb9f37ddc8e0169aed2a047fd00';
+
+/// See also [Authentication].
+@ProviderFor(Authentication)
+final authenticationProvider =
+    NotifierProvider<Authentication, AuthenticationEntity>.internal(
+      Authentication.new,
+      name: r'authenticationProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$authenticationHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$Authentication = Notifier<AuthenticationEntity>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
