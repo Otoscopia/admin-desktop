4a1f626db2d33021045f1b8fa7b2349f28730e96
Usha Nicole Cobrado <ushanicole0421@gmail.com>
2025-05-03 14:51:00 +0800

    ✨ feat: Add fetch team roles provider

---
+++ lib/src/feature/roles/provider/fetch_team_roles.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'fetch_team_roles.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$fetchTeamsRolesHash() => r'c1db6de5f91a8a3bcbcc50648963204550d9d540';
+
+/// See also [fetchTeamsRoles].
+@ProviderFor(fetchTeamsRoles)
+final fetchTeamsRolesProvider =
+    AutoDisposeFutureProvider<Map<String, dynamic>>.internal(
+      fetchTeamsRoles,
+      name: r'fetchTeamsRolesProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$fetchTeamsRolesHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef FetchTeamsRolesRef = AutoDisposeFutureProviderRef<Map<String, dynamic>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
